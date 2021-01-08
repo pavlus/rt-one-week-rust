2853ae414d4a4c84e6dbdafef9e3ed25a19606e5
Pavel Remygailo <pavel.remygailo+github@gmail.com>
2021-01-08 07:50:18 +0200

    Cleanup ConstantMedium

--- src/hittable/constant_medium.rs
+++ src/hittable/constant_medium.rs
@@ -30,34 +30,30 @@ impl<B:Hittable, T: Texture> ConstantMedium<B, Isotropic<T>> {
 
 impl<B:Hittable, M: Material> Hittable for ConstantMedium<B,M> {
     fn hit(&self, ray_ctx: &RayCtx, dist_min: Distance, dist_max: Distance) -> Option<Hit> {
-        self.boundary.hit(ray_ctx, Distance::MIN, Distance::MAX).and_then(|enter_hit| {
-            self.boundary.hit(ray_ctx, enter_hit.dist + 0.001, Distance::MAX).and_then(|exit_hit| {
-                let enter_dist = Distance::max(dist_min, enter_hit.dist);
-                let exit_dist = Distance::min(exit_hit.dist, dist_max);
-                if enter_dist >= exit_dist {
-                    return None
-                }
+        let enter_hit = self.boundary.hit(ray_ctx, Distance::MIN, Distance::MAX)?;
+        let exit_hit = self.boundary.hit(ray_ctx, enter_hit.dist + 0.001, Distance::MAX)?;
+        let enter_dist = Distance::max(dist_min, enter_hit.dist);
+        let exit_dist = Distance::min(exit_hit.dist, dist_max);
+        if enter_dist >= exit_dist {
+            return None
+        }
 
-                let ray = ray_ctx.ray;
-                // TODO: describe why such distribution?
-                //  isotropic scattering follows Poisson point process?
-                let hit_dist: Distance = next::<Distance, rand_distr::Exp1>(rand_distr::Exp1) / self.density;
-                let dir_norm = ray.direction.norm();
-                let inner_travel_distance = (exit_dist - enter_dist) * dir_norm;
-                if hit_dist >= inner_travel_distance {
-                    return None
-                }
-
-                let dist = enter_dist + hit_dist / dir_norm;
-                Some(Hit::new(
-                    dist,
-                    ray.point_at(dist),
-                    Unit::new_unchecked(rand_in_unit_sphere().coords),
-                    &self.phase_function,
-                    enter_hit.u, enter_hit.v,
-                ))
-            })
-        })
+        // random walk which follows Poisson point process, using exponential distribution
+        let hit_dist: Distance = next::<Distance, rand_distr::Exp1>(rand_distr::Exp1) / self.density;
+        let inner_travel_distance = exit_dist - enter_dist;
+        if hit_dist < inner_travel_distance {
+            let dist = enter_dist + hit_dist;
+            let ray = &ray_ctx.ray;
+            Some(Hit::new(
+                dist,
+                ray.point_at(dist),
+                Unit::new_unchecked(V3::x()),
+                &self.phase_function,
+                enter_hit.u, enter_hit.v,
+            ))
+        } else {
+            None
+        }
     }
 
     #[inline]
